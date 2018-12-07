af2afbe66b64f202c48f7ddbe5b9892b4a3388a4
nathanlschneider <n.l.schneider@gmail.com>
2018-12-06 21:20:46 -0500

    added about me section

--- src/Layouts/App/AboutMe/AboutMe.js
+++ src/Layouts/App/AboutMe/AboutMe.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import './AboutMe.scss';
 export default class AboutMe extends Component {
     render() {
-        return <div className="bigBody">HELLO</div>;
+        return (
+            <div className="about-container">
+                <div className="about-title">
+                    <span>About Me</span>
+                </div>
+                <div className="about-content">
+                    <div className="about-left" />
+                    <div className="about-right">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus ipsum a ligula
+                            pulvinar, gravida convallis massa porta. Quisque libero sem, cursus et vestibulum ut, ornare
+                            sit amet sem. Nunc accumsan nisi id ipsum imperdiet commodo. Nullam odio odio, laoreet et
+                            vestibulum ac, tincidunt nec ipsum.
+                        </p>
+                    </div>
+                </div>
+                <div className="about-footer">
+                    <div>Resume</div>
+                    <div>LinkedIn</div>
+                    <div>Contact</div>
+                </div>
+            </div>
+        );
     }
 }
